0657c2602c9f96722d666b8e572682e409064f2a
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-05-28 18:55:14 +0200

    Created functions

--- contacts.js
+++ contacts.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
 const contactsPath = path.join(
@@ -7,4 +7,61 @@ const contactsPath = path.join(
   "contacts.json"
 );
 
-console.log(contactsPath);
+// TODO: document each function
+async function listContacts() {
+  const contactList = await fs
+    .readFile(contactsPath)
+    .then((data) => data.toString());
+
+  console.log(contactList);
+}
+
+async function getContactById(contactId) {
+  const contactList = await fs
+    .readFile(contactsPath)
+    .then((data) => data.toString());
+
+  const contactArray = JSON.parse(contactList);
+
+  const foundContact = contactArray.find((contact) => contact.id === contactId);
+
+  console.log(foundContact);
+}
+
+async function removeContact(contactId) {
+  const contactList = await fs
+    .readFile(contactsPath)
+    .then((data) => data.toString());
+
+  const contactArray = JSON.parse(contactList);
+
+  const newContacts = contactArray.filter(
+    (contact) => contact.id !== contactId
+  );
+
+  const parsedNew = JSON.stringify(newContacts);
+
+  fs.writeFile(contactsPath, parsedNew);
+  console.log("Removal successful");
+}
+
+async function addContact(name, email, phone) {
+  const contactList = await fs
+    .readFile(contactsPath)
+    .then((data) => data.toString());
+
+  const id = JSON.stringify(Date.now());
+
+  const contactArray = JSON.parse(contactList);
+
+  contactArray.push({ id, name, email, phone });
+  const arrayJson = JSON.stringify(contactArray);
+  fs.writeFile(contactsPath, arrayJson);
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-console.log("test");
-
+const contacts = require("./contacts");
